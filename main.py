7ba54e508a74e681a1b8e324673567527fe0b33b
akshay-1612 <akshayk.cs17@sahyadri.edu.in>
2020-09-21 16:17:59 +0530

    changed file location

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         notification.notify(
             title = "DRINK WATER",  # Title of the Notification
             message = "Hey friend, It's time to drink water",   # Message of the Notification
-            app_icon = 'C:\\Users\\admin\\Desktop\\Python Projects\\water notification\\index.ico',  # Icon to be displayed along with the message
+            app_icon = 'Drink-Water-Notification/index.ico',  # Icon to be displayed along with the message
             timeout = 10  #notification remains on screen for 10 seconds
         )
 
