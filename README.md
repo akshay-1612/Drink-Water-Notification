c2fa9eeaf2c60dcf5a08ee976ebfdc363118dd7a
akshay-1612 <akshayk.cs17@sahyadri.edu.in>
2020-09-21 16:03:46 +0530

    added program file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Drink-Water-Notification
+Sends Notification to drink water every 2 hours    
+You can schedule this program to run automatically using ***Windows Task Scheduler***    
---
+++ main.py
@@ -0,0 +1,14 @@
+from plyer import notification
+import time
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = "DRINK WATER",  # Title of the Notification
+            message = "Hey friend, It's time to drink water",   # Message of the Notification
+            app_icon = 'C:\\Users\\admin\\Desktop\\Python Projects\\water notification\\index.ico',  # Icon to be displayed along with the message
+            timeout = 10  #notification remains on screen for 10 seconds
+        )
+
+        time.sleep(120*60)  # every 10 seconds send the notification
+        
